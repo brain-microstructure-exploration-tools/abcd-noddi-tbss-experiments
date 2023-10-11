4db1dad8c9fb866034372c2bc52e6f88cda47f9c
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2023-10-11 16:02:00 -0400

    investigate dipy vs mrtrix response fn
    for #1

--- dipy_response_function_experiments.py
+++ dipy_response_function_experiments.py
@@ -34,6 +34,7 @@ from dipy.core.gradients import gradient_table
 from common import get_unique_file_with_extension
 from dipy.reconst.csdeconv import recursive_response
 from dipy.reconst.csdeconv import ConstrainedSphericalDeconvModel
+from dipy.reconst.csdeconv import AxSymShResponse
 import numpy as np
 
 # %%
@@ -44,5 +45,83 @@ output_dir = Path('csd_output_dipy/')
 output_dir_fod = output_dir/'fod'
 output_dir_fod.mkdir(exist_ok=True)
 
+# %%
+dwi_nii_directory = np.random.choice(list(extracted_images_path.glob('*/*/*/dwi/')))
+
+# %%
+nii_path = get_unique_file_with_extension(dwi_nii_directory, 'nii')
+print(f"INPUT_DIR={dwi_nii_directory}")
+print(f"STEM={nii_path.stem}")
+subject_output_file = output_dir_fod/(nii_path.stem + '_fod.nii.gz')
+if subject_output_file.exists():
+        print(f"Warning: output for subject {nii_path.stem} already exists at the file\n\t{subject_output_file}")
+
+bval_path = get_unique_file_with_extension(dwi_nii_directory, 'bval')
+bvec_path = get_unique_file_with_extension(dwi_nii_directory, 'bvec')
+bvals, bvecs = read_bvals_bvecs(str(bval_path), str(bvec_path))
+gtab = gradient_table(bvals, bvecs)
+
+mask_path = masks_path/(nii_path.stem + '_mask.nii.gz')
+
+data, affine, img = load_nifti(str(nii_path), return_img=True)
+mask_data, mask_affine, mask_img = load_nifti(str(mask_path), return_img=True)
+
+subject_dti_dir = dti_path/(nii_path.stem)
+if not subject_dti_dir.exists():
+    raise Exception(f"Could not find directory {subject_dti_dir}. Did you complete the DTI fit for this subject?")
+fa_path = subject_dti_dir/(nii_path.stem + '_fa.nii.gz')
+fa_data, fa_affine, fa_img = load_nifti(str(fa_path), return_img=True)
+
+md_path = subject_dti_dir/(nii_path.stem + '_md.nii.gz')
+md_data, md_affine, md_img = load_nifti(str(md_path), return_img=True)
+
+# %%
+# Warning this cell takes a long time to run and is not needed for the things that follow.
+
+# below approach and parameters are taken from the example https://dipy.org/documentation/1.1.0./examples_built/reconst_csd/
+wm_mask = (np.logical_or(fa_data >= 0.4, (np.logical_and(fa_data >= 0.15, md_data >= 0.0011))))
+response = recursive_response(gtab, data, mask=wm_mask, sh_order=8, peak_thr=0.01, init_fa=0.08, init_trace=0.0021, iter=2, convergence=0.1, parallel=True)
+
+# %%
+with open(Path('csd_output_mrtrix/average_response.txt')) as f:
+    mrtrix_response_function_string = f.readlines()[-1]
+list(map(float,mrtrix_response_function_string.strip().split(' ')))
+
+# %%
+response_mrtrix = AxSymShResponse(300,np.array(list(map(float,mrtrix_response_function_string.strip().split(' ')))))
+
 # %% [markdown]
+# The 300 looks like it's coming from nowhere, but actually in dipy, the `response.S0` is only saved [here](https://github.com/dipy/dipy/blob/1d558fce90d38e71cc16ed4c7e48bf1116ae459b/dipy/reconst/csdeconv.py#L262) when you construct the `ConstrainedSphericalDeconvModel` object. When you call the `fit` method you can see [here](https://github.com/dipy/dipy/blob/1d558fce90d38e71cc16ed4c7e48bf1116ae459b/dipy/reconst/csdeconv.py#L289-L293) that the S0 value not used anywhere. It's only used in the `predict` method, which we are not really using here because we are not trying to simulate signals right now. So I think for the CSD fit to generate a FOD, the S0 value doesn't really matter.
+
+# %%
+csd_model = ConstrainedSphericalDeconvModel(gtab, response_mrtrix, sh_order=8)
+
+# %%
+csd_fit = csd_model.fit(data, mask=mask_data)
+
+# %%
+subject_output_file_alt = subject_output_file.parent / f"{subject_output_file.name.split('.')[0]}_mrtrixResponse.nii.gz"
+subject_output_file_alt
+
+# %%
+csd_fit
+
+# %%
+csd_shm_coeff = csd_fit.shm_coeff
+save_nifti(subject_output_file_alt, csd_shm_coeff, affine, img.header)
+print(f'saved {subject_output_file_alt}')
+
+# %% [markdown]
+# I visually inspected three images using `mrview`:
+# - the mrtrix reconstruction using the mrtrix estimated average response function
+# - the dipy reconstruction using the dipy estimated average response function
+# - the dipy reconstruction using the mrtrix estimated average response function
 #
+# The latter two were both pretty bad looking (e.g. qualtiatively when looking at corpus callosum where the FODs should be extra sharp and clean) so I don't think my issue is only with response function generation.
+#
+# Maybe the dipy spherical harmonic basis order is different? So when I save the 45-channel coefficients list perhaps it's being misinterpreted by mrview? Based on [my investigation](https://github.com/dipy/dipy/discussions/2861#discussioncomment-7256439), no:
+
+# %%
+from dipy.reconst.shm import sph_harm_ind_list
+m,l = sph_harm_ind_list(8)
+print(list(zip(l,m)))
