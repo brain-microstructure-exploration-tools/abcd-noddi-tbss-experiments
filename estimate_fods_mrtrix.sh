152d284bf573bd9a7953fe95d9f7b04a2cb09bff
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-01-09 20:31:23 -0500

    reintroduce dhollander dwi2response
    This is needed if we want to use msmt_csd later, because
    we need a response function for each shell

--- estimate_fods_mrtrix.sh
+++ estimate_fods_mrtrix.sh
@@ -14,10 +14,14 @@ MASKS_PATH=$2
 DESTINATION_PATH=$3
 
 RESPONSE_PATH=$DESTINATION_PATH/response_functions
-AVERAGE_RESPONSE=$DESTINATION_PATH/average_response.txt
+AVERAGE_RESPONSE_WM=$DESTINATION_PATH/average_response_wm.txt
+AVERAGE_RESPONSE_GM=$DESTINATION_PATH/average_response_gm.txt
+AVERAGE_RESPONSE_CSF=$DESTINATION_PATH/average_response_csf.txt
 FOD_PATH=$DESTINATION_PATH/fod
 
-mkdir -p $RESPONSE_PATH
+mkdir -p $RESPONSE_PATH/WM
+mkdir -p $RESPONSE_PATH/GM
+mkdir -p $RESPONSE_PATH/CSF
 mkdir -p $FOD_PATH
 
 # Estimate a response function for each image
@@ -27,13 +31,15 @@ for DWI_PATH in "$EXTRACTED_IMAGES_PATH"/*/*/*/dwi/; do
   BVEC=$DWI_PATH/*.bvec
   BASENAME=$(basename $DWI .nii)
   MASK=$MASKS_PATH/${BASENAME}_mask.nii.gz
-  dwi2response tournier $DWI $RESPONSE_PATH/$BASENAME.txt -fslgrad $BVEC $BVAL -mask $MASK
+  dwi2response dhollander $DWI $RESPONSE_PATH/WM/$BASENAME.txt $RESPONSE_PATH/GM/$BASENAME.txt $RESPONSE_PATH/CSF/$BASENAME.txt -fslgrad $BVEC $BVAL -mask $MASK
 done
 
 # Taking advice from
 # https://mrtrix.readthedocs.io/en/latest/fixel_based_analysis/st_fibre_density_cross-section.html?highlight=average_response#computing-an-average-white-matter-response-function
 # we compute an average response function
-responsemean $RESPONSE_PATH/*.txt $AVERAGE_RESPONSE
+responsemean $RESPONSE_PATH/WM/*.txt $AVERAGE_RESPONSE_WM
+responsemean $RESPONSE_PATH/GM/*.txt $AVERAGE_RESPONSE_GM
+responsemean $RESPONSE_PATH/CSF/*.txt $AVERAGE_RESPONSE_CSF
 
 # Estimate a FOD for each image
 for DWI_PATH in "$EXTRACTED_IMAGES_PATH"/*/*/*/dwi/; do
@@ -42,5 +48,5 @@ for DWI_PATH in "$EXTRACTED_IMAGES_PATH"/*/*/*/dwi/; do
   BVEC=$DWI_PATH/*.bvec
   BASENAME=$(basename $DWI .nii)
   MASK=$MASKS_PATH/${BASENAME}_mask.nii.gz
-  dwi2fod msmt_csd $DWI $AVERAGE_RESPONSE $FOD_PATH/${BASENAME}_fod.nii.gz -fslgrad $BVEC $BVAL -mask $MASK
+  dwi2fod msmt_csd $DWI $AVERAGE_RESPONSE_WM $FOD_PATH/${BASENAME}_fod.nii.gz -fslgrad $BVEC $BVAL -mask $MASK
 done
