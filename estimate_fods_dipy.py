adc0cf9b64375b2d84063b80da50c6771025d957
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-01-28 22:05:40 -0500

    group response functions by study site
    close #4

--- estimate_fods_dipy.py
+++ estimate_fods_dipy.py
@@ -9,6 +9,7 @@ from dipy.reconst.shm import convert_sh_descoteaux_tournier
 import numpy as np
 from dipy.reconst.csdeconv import mask_for_response_ssst, response_from_mask_ssst
 import warnings
+import pandas as pd
 
 # === Parse args ===
 
@@ -21,10 +22,11 @@ extracted_images_path = Path(args.extracted_images_path)
 masks_path = Path(args.masks_path)
 output_dir = Path(args.output_dir)
 output_dir_fod = output_dir/'fod'
-output_dir_response = output_dir/'response_functions'
-average_response_path = output_dir/'average_response.txt'
+output_dir_response = output_dir/'subject_response_functions'
+average_response_dir = output_dir/'group_response_functions'
 output_dir_fod.mkdir(exist_ok=True)
 output_dir_response.mkdir(exist_ok=True)
+site_df = pd.read_csv(extracted_images_path/'site_table.csv')
 
 # === Iterate through images, estimating response functions. ===
 
@@ -33,7 +35,7 @@ for dwi_nii_directory in extracted_images_path.glob('*/*/*/dwi/'):
     nii_path = get_unique_file_with_extension(dwi_nii_directory, 'nii.gz')
     basename = nii_path.name.split('.')[0]
 
-    response_output_file = output_dir_response/(basename + '_response.txt')
+    response_output_file = output_dir_response/(basename + '.txt')
     if response_output_file.exists():
         print(f"Skipping response function estimate for {basename} since the following output file exists:\n{response_output_file}")
         continue
@@ -88,8 +90,12 @@ for dwi_nii_directory in extracted_images_path.glob('*/*/*/dwi/'):
     write_dipy_response(response, response_output_file)
 
 # === Aggregate response functions ===
-print("aggregating estimated response functions into one common response function....")
-aggregate_dipy_response_functions_workflow(output_dir_response, average_response_path)
+print("aggregating estimated response functions by study site....")
+for site,group in site_df.groupby('site_id_l'):
+    aggregate_dipy_response_functions_workflow(
+        [output_dir_response/(basename + '.txt') for basename in group.basename],
+        average_response_dir/(str(site)+'.txt')
+    )
 
 
 # === Iterate through images, performing CSD to compute FODs. ===
@@ -116,10 +122,11 @@ for dwi_nii_directory in extracted_images_path.glob('*/*/*/dwi/'):
     data, affine, img = load_nifti(str(nii_path), return_img=True)
     mask_data, mask_affine, mask_img = load_nifti(str(mask_path), return_img=True)
 
-    print(f"applying CSD to estimate FOD for {basename}...")
+    site = site_df[site_df.basename == basename].site_id_l.item()
+    print(f"applying CSD to estimate FOD for {basename} (site {site})...")
 
     # below approach and parameters are taken from the example https://dipy.org/documentation/1.1.0./examples_built/reconst_csd/
-    response = read_dipy_response(average_response_path)
+    response = read_dipy_response(average_response_dir/(str(site)+'.txt'))
     csd_model = ConstrainedSphericalDeconvModel(gtab, response, sh_order=8)
     csd_fit = csd_model.fit(data, mask=mask_data)
 
