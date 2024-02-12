brain-microstructure-exploration-tools/abcd-noddi-tbss-experiments

commit bd3761f6e7841619242fa51387cfe00dedb41b06

update extraction script to work with the Phillips scans
This was my first attempt, but actually it's not quite right.
Because I am extracting each archive file into its own directory
(as opposed to extracting them all to the same target directory)
the phillips scans won't end up as two images in the same directory
as the current change assumes. This will be fixed in a future commit.

extract_images.py
@@ -24,7 +24,9 @@ output_path.mkdir(exist_ok=True)
 df = pd.read_csv(table_path)
 filenames = []
 basenames = []
-for filename in df.filename:
+for i,row in df.iterrows():
+    filename = row.filename
+    is_phillips = row.mri_info_manufacturer == "Philips Medical Systems"
 
     archive_file_path = archive_path/filename
     extraction_target = output_path/(Path(filename).stem)
@@ -45,9 +47,17 @@ for filename in df.filename:
     img_path_list = nii_path_list + niigz_path_list
     if len(img_path_list) == 0:
         raise FileNotFoundError(f"Couldn't find any image file in {extraction_target}")
-    if len(img_path_list) > 1:
+    if (len(img_path_list) > 1 and not is_phillips) or (len(img_path_list) > 2 and is_phillips):
         img_path_list_formatted = '\n\t'.join(map(str,img_path_list))
-        raise Exception(f"Multiple image files were found in {extraction_target}:\n\t{img_path_list_formatted}")
+        raise Exception(f"Unexpected multiple image files were found in {extraction_target}:\n\t{img_path_list_formatted}")
+    if len(img_path_list) == 2 and is_phillips:
+        # In this case one of the files was already processed and converted to nii.gz, so by removing it we should reduce to the
+        # case where img_path_list has a unique element (an .nii or another also-processed .nii.gz)
+        if len(niigz_path_list) < 1:
+            raise Exception(f"Unexpected situation: Two image files are present as they should be for a Phillips scan, however they were both extracted to .nii without being recompressed to .nii.gz. This should not have happened. Consider deleting {extraction_target} and trying again.")
+        img_path_list = [p for p in img_path_list if p!=niigz_path_list[0]]
+        assert(len(img_path_list)==1)
+        print("Working on the second half of a phillips scan :D") # TODO: remove this line. it's just for testing
     img_file_path = img_path_list[0]
     basename = img_file_path.name.split('.')[0]
     filenames.append(filename)

estimate_fods_dipy.py
@@ -33,6 +33,8 @@ site_df = pd.read_csv(extracted_images_path/'site_table.csv')
 for dwi_nii_directory in extracted_images_path.glob('*/*/*/dwi/'):
 
     nii_path = get_unique_file_with_extension(dwi_nii_directory, 'nii.gz')
+    # (Note that getting a unique file like this wouldn't work in general on an ABCD download if someone extracted everything to the same
+    # target folder instead of creating one folder for each archive as I did.)
     basename = nii_path.name.split('.')[0]
 
     response_output_file = output_dir_response/(basename + '.txt')

fit_dti.py
@@ -25,6 +25,8 @@ output_dir = Path(args.output_dir)
 for dwi_nii_directory in extracted_images_path.glob('*/*/*/dwi/'):
 
     nii_path = get_unique_file_with_extension(dwi_nii_directory, 'nii.gz')
+    # (Note that getting a unique file like this wouldn't work in general on an ABCD download if someone extracted everything to the same
+    # target folder instead of creating one folder for each archive as I did.)
     basename = nii_path.name.split('.')[0]
 
     subject_output_dir = output_dir/basename

fit_watson_noddi.py
@@ -41,6 +41,8 @@ NODDI_mod.set_fixed_parameter('G1Ball_1_lambda_iso', 3e-9)
 for dwi_nii_directory in extracted_images_path.glob('*/*/*/dwi/'):
 
     nii_path = get_unique_file_with_extension(dwi_nii_directory, 'nii.gz')
+    # (Note that getting a unique file like this wouldn't work in general on an ABCD download if someone extracted everything to the same
+    # target folder instead of creating one folder for each archive as I did.)
     basename = nii_path.name.split('.')[0]
 
     subject_output_dir = output_dir/basename

generate_b0_averages.py
@@ -15,6 +15,8 @@ output_dir = Path(args.output_dir)
 for dwi_nii_directory in extracted_images_path.glob('*/*/*/dwi/'):
 
     nii_path = get_unique_file_with_extension(dwi_nii_directory, 'nii.gz')
+    # (Note that getting a unique file like this wouldn't work in general on an ABCD download if someone extracted everything to the same
+    # target folder instead of creating one folder for each archive as I did.)
     bval_path = get_unique_file_with_extension(dwi_nii_directory, 'bval')
     bvec_path = get_unique_file_with_extension(dwi_nii_directory, 'bvec')
 

generate_nrrd_headers.py
@@ -10,6 +10,8 @@ extracted_images_path = Path(args.extracted_images_path)
 
 for dwi_nii_directory in extracted_images_path.glob('*/*/*/dwi/'):
     nii_file_path = get_unique_file_with_extension(dwi_nii_directory, 'nii.gz')
+    # (Note that getting a unique file like this wouldn't work in general on an ABCD download if someone extracted everything to the same
+    # target folder instead of creating one folder for each archive as I did.)
     output_nhdr_path = dwi_nii_directory/(nii_file_path.stem + '.nhdr')
     conversion.nhdr_write(
         str(nii_file_path),

commit 14e52b7d729df3773215a2db55f7c7179d621b49

remove handling of extraneous case in extract_images
This fixes the incorrect approach introduced in bd3761f6e78416.
Because we are extracting each archive into a unique folder, images
don't end up together in the same folder in the case of a multi part
Philips scan.

extract_images.py
@@ -24,9 +24,7 @@ output_path.mkdir(exist_ok=True)
 df = pd.read_csv(table_path)
 filenames = []
 basenames = []
-for i,row in df.iterrows():
-    filename = row.filename
-    is_phillips = row.mri_info_manufacturer == "Philips Medical Systems"
+for filename in df.filename:
 
     archive_file_path = archive_path/filename
     extraction_target = output_path/(Path(filename).stem)
@@ -47,17 +45,12 @@ for i,row in df.iterrows():
     img_path_list = nii_path_list + niigz_path_list
     if len(img_path_list) == 0:
         raise FileNotFoundError(f"Couldn't find any image file in {extraction_target}")
-    if (len(img_path_list) > 1 and not is_phillips) or (len(img_path_list) > 2 and is_phillips):
+    if len(img_path_list) > 1:
         img_path_list_formatted = '\n\t'.join(map(str,img_path_list))
-        raise Exception(f"Unexpected multiple image files were found in {extraction_target}:\n\t{img_path_list_formatted}")
-    if len(img_path_list) == 2 and is_phillips:
-        # In this case one of the files was already processed and converted to nii.gz, so by removing it we should reduce to the
-        # case where img_path_list has a unique element (an .nii or another also-processed .nii.gz)
-        if len(niigz_path_list) < 1:
-            raise Exception(f"Unexpected situation: Two image files are present as they should be for a Phillips scan, however they were both extracted to .nii without being recompressed to .nii.gz. This should not have happened. Consider deleting {extraction_target} and trying again.")
-        img_path_list = [p for p in img_path_list if p!=niigz_path_list[0]]
-        assert(len(img_path_list)==1)
-        print("Working on the second half of a phillips scan :D") # TODO: remove this line. it's just for testing
+        raise Exception(f"Multiple image files were found in {extraction_target}:\n\t{img_path_list_formatted}")
+        # (Note that multiple images could happen in general on an ABCD download if someone extracted everything to the same
+        # target folder instead of creating one folder for each archive as I did. Because the Phillips scans would be in two parts
+        # and those would end up together in the same subriectory.)
     img_file_path = img_path_list[0]
     basename = img_file_path.name.split('.')[0]
     filenames.append(filename)